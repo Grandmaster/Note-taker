b7493ad46d9d380520cc59017212681f33fcccf4
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-04-03 16:04:44 -0400

    Changed file structure to get application to deploy on heroku

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(express.json());
 
 // Page-displaying Routes
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "/notes.html"));
 });
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 // GET Routes
@@ -77,7 +77,7 @@ app.delete("/api/notes/:id", function (req, res) {
 
 // Catch-all route
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 // Listening to port
